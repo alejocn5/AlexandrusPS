187e39168163ac5420dea24fbcc489dee8f10d39
vivienschoonenberg <vivienschoonenberg@gmail.com>
2023-08-30 08:57:15 -0400

    fix APS3 by Alejandro

--- AlexandrusPS_Positive_selection_pipeline/Code/APS3_CleanProteinOrthoTabe.R
+++ AlexandrusPS_Positive_selection_pipeline/Code/APS3_CleanProteinOrthoTabe.R
@@ -1,24 +1,20 @@
 library(dplyr)
-#library(ggplot2)
-#library(caret)
-#library(reshape2)
-#library(ggpubr)
-#library(RColorBrewer)
-#library(stringr)
-#library(viridis)
-#library(ggpubr)
+# library(ggplot2)
+# library(caret)
+# library(reshape2)
+# library(ggpubr)
+# library(RColorBrewer)
+# library(stringr)
+# library(viridis)
+# library(ggpubr)
 
 POGF_all <- read.delim("ProteinOrthoTable.proteinortho.tsv")
 
-POGF_all$ID<-c(seq(1, nrow(POGF_all)))
+POGF_all$ID <- c(seq(1, nrow(POGF_all)))
 colnames(POGF_all)[1] <- "A"
 colnames(POGF_all)[2] <- "B"
 colnames(POGF_all)[3] <- "C"
-POGF_all <-  head(select(POGF_all, -A, -B, -C))
-              
-
-write.table(POGF_all, file = "ProteinOrthoTable.proteinortho.fill", sep = "\t", quote = FALSE, row.names = F,  col.names=FALSE)
+POGF_all_fin <- POGF_all %>% select(-(1:3))
 
 
-
-
+write.table(POGF_all_fin, file = "ProteinOrthoTable.proteinortho.fill", sep = "\t", quote = FALSE, row.names = F, col.names = FALSE)
